d6e7c5161f4842e8162184f7292e386bb6f12ac2
kimdowon <kdw030612@gmail.com>
2022-03-16 11:17:39 +0900

    2022 03 16

---
+++ src/main/java/Exam01.java
@@ -0,0 +1,9 @@
+public class Exam01 {
+    public static void main(String[] args) {
+        System.out.println("2220110006 김도원");
+        System.out.println("김포시 장기동");
+        System.out.println("section의 기본 01 system.out의 기본");
+        System.out.println("클래스 안에만 존재해야한다"
+        );
+    }
+}
