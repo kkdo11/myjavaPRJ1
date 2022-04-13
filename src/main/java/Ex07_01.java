dbc8b5de6097af9045050e7542c538268774beda
kimdowon <kdw030612@gmail.com>
2022-04-13 10:57:00 +0900

    20220411

---
+++ src/main/java/Ex07_01.java
@@ -0,0 +1,13 @@
+public class Ex07_01 {
+    public static void main(String[] args) {
+        int i;
+        i = 0;
+
+        while (i < 9) {
+            System.out.printf("while 문을 공부 중입니다 \n");
+
+            i++;
+
+        }
+    }
+}
---
+++ src/main/java/Ex07_03.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Ex07_03 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a, b;
+
+        while (true) {
+            System.out.printf("더할 첫 번째 수 입력 :");
+            a = in.nextInt();
+            System.out.printf("더할 두 번째 수 입력 :");
+            b = in.nextInt();
+
+            System.out.printf("%d+%d=%d \n", a, b, a + b);
+        }
+    }
+}
