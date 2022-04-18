606e31ad52dc52bbeb7596d121bd49d9995c474f
kimdowon <kdw030612@gmail.com>
2022-04-18 22:21:12 +0900

    20220418

---
+++ src/main/java/Bj2742.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Bj2742 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int N = in.nextInt();
+        in.close();
+
+
+
+        while(N > 0) {
+            System.out.println(N);
+            N--;
+        }
+    }
+}
+ 
