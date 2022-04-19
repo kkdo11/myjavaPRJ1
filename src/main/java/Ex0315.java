7c9175bd6b5a86f0cb749c11d2bdd0c979026bb0
kimdowon <kdw030612@gmail.com>
2022-04-19 10:43:53 +0900

    20220419

---
+++ src/main/java/Ex0315.java
@@ -0,0 +1,13 @@
+public class Ex0315 {
+    public static void main(String[] args) {
+        String str1= "IT OookBok 입니다";
+        String str2= "10";
+        String str3= "20";
+
+        str1= "JAVA입니다";
+
+        System.out.printf("%s \n", str1);
+        System.out.printf("%s \n", str2+str3);
+
+    }
+}
