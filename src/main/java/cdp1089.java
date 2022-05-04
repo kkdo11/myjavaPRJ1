d402b0f4633fb6a19db9f0e39f2db2739cc1e32e
kimdowon <kdw030612@gmail.com>
2022-05-04 09:19:23 +0900

    20220504

---
+++ src/main/java/cdp1089.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class cdp1089 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+
+        int a=in.nextInt();
+        int d=in.nextInt();
+        int n=in.nextInt();
+
+
+
+        for(int i=1; i<n; i++)
+            a+=d;
+
+        System.out.printf("%d",a);
+
+    }
+}
