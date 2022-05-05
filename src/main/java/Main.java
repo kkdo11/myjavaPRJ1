c7fde7a6b89f3677a3e50109d0d81b4a927aacdf
kimdowon <kdw030612@gmail.com>
2022-05-05 20:58:52 +0900

    20220505

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
+import java.util.Arrays;
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        System.out.printf("\"ddddd\"");
+        Scanner in =new Scanner(System.in);
+         int n=in.nextInt();
 
+         int aa[]=new int[n];
 
+         for (int i=0; i<n; i++)
+             aa[i]=in.nextInt();
 
+        Arrays.sort(aa);
+        System.out.println(aa[0]+" "+aa[n-1]);
     }
 }
