52930ab38170cf22180f998c1aa16fb24e63267b
kimdowon <kdw030612@gmail.com>
2022-05-11 10:12:08 +0900

    20220510

--- src/main/java/framework.java
+++ src/main/java/framework.java
@@ -3,19 +3,18 @@ import java.util.List;
 
 public class framework {
     public static void main(String[] args) {
-        List<String> list= new ArrayList<>();//무조건 외우기!!!!!!!!
+        List<String> list = new ArrayList<>();//무조건 외우기!!!!!!!!
 
         list.add("이협건");
         list.add("홍길동");
         list.add("임꺽정");
         list.add("고길동");
 
-        int listSize= list.size();
+        int listSize = list.size();
 
-        for (String name : list) {
-            System.out.println("이름 :" +name);
-
+        list.forEach(name -> System.out.println("name :" + name));
 
 
-    }}
+    }
 }
+
