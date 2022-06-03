a6d0f2236285ba3a94dafec4c255bc34da03efb5
kimdowon <kdw030612@gmail.com>
2022-06-03 18:02:38 +0900

    Changes

---
+++ src/main/java/cdp1065.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class cdp1065 {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a,b,c;
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+
+        if (a%2==0)
+            System.out.printf("%d",a);
+        if (b%2==0)
+            System.out.printf("%d",b);
+        if (c%2==0)
+            System.out.printf("%d",c);
+
+    }
+}
