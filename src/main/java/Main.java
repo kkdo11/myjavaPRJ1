766151cdfacb1be1098b3bd6564f9ca9c1b63d76
kimdowon <kdw030612@gmail.com>
2022-06-10 13:54:52 +0900

    Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,19 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner s = new Scanner(System.in);
-        int r = s.nextInt();
-        int g = s.nextInt();
-        int b = s.nextInt();
-        int n = 0;
-        for (int i = 0; i < r; i++) {
-            for (int k = 0; k < g; k++) {
-                for (int j = 0; j < b; j++) {
-                    System.out.printf("%d %d %d\n", i, k, j);
-                    n += 1;
-                }
-            }
+        Scanner s=new Scanner(System.in);
+
+        int a=s.nextInt();
+        int d=s.nextInt();
+        int n=s.nextInt();
+        int sum = a;
+
+        for (int i=1; i<n; i++) {
+            sum=sum+ d;
         }
-        System.out.println(n);
+        System.out.println(sum);
     }
 }
