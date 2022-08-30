7d60b7918a291b88698f511922afb6a2538cfb2a
kimdowon <kdw030612@gmail.com>
2022-08-30 22:06:33 +0900

    20220230

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int c=s.nextInt();
+        int d=s.nextInt();
 
+        if (a+b+c+d==0)
+            System.out.println("모");
+        else if (a+b+c+d==1)
+            System.out.println("도");
+        else if (a+b+c+d==2)
+            System.out.println("개");
+        else if (a+b+c+d==3)
+            System.out.println("걸");
+        else if (a+b+c+d==4)
+            System.out.println("윷");
     }
 }
