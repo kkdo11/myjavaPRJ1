c35d1cd31c2a59654359bbc624c617163bf9b3ee
kimdowon <kdw030612@gmail.com>
2022-11-13 13:42:22 +0900

    20221113

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,21 +1,90 @@
 import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Scanner s=new Scanner(System.in);
-        int a=s.nextInt();
-        int b=s.nextInt();
-        int c=s.nextInt();
-        int d=s.nextInt();
+        Scanner in = new Scanner(System.in);
 
-        if (a+b+c+d==0)
-            System.out.println("모");
-        else if (a+b+c+d==1)
-            System.out.println("도");
-        else if (a+b+c+d==2)
-            System.out.println("개");
-        else if (a+b+c+d==3)
-            System.out.println("걸");
-        else if (a+b+c+d==4)
-            System.out.println("윷");
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+        if (a % 400 == 0 || ((a % 4 == 0) && (a % 100 != 0))) {
+            switch (b) {
+                case 1:
+                    System.out.println(31);
+                    break;
+                case 2:
+                    System.out.println(29);
+                    break;
+                case 3:
+                    System.out.println(31);
+                    break;
+                case 4:
+                    System.out.println(30);
+                    break;
+                case 5:
+                    System.out.println(31);
+                    break;
+                case 6:
+                    System.out.println(30);
+                    break;
+                case 7:
+                    System.out.println(31);
+                    break;
+                case 8:
+                    System.out.println(31);
+                    break;
+                case 9:
+                    System.out.println(30);
+                    break;
+                case 10:
+                    System.out.println(31);
+                    break;
+                case 11:
+                    System.out.println(30);
+                    break;
+                case 12:
+                    System.out.println(31);
+                    break;
+            }
+        } else {
+            switch (b) {
+                case 1:
+                    System.out.println(31);
+                    break;
+                case 2:
+                    System.out.println(28);
+                    break;
+                case 3:
+                    System.out.println(31);
+                    break;
+                case 4:
+                    System.out.println(30);
+                    break;
+                case 5:
+                    System.out.println(31);
+                    break;
+                case 6:
+                    System.out.println(30);
+                    break;
+                case 7:
+                    System.out.println(31);
+                    break;
+                case 8:
+                    System.out.println(31);
+                    break;
+                case 9:
+                    System.out.println(30);
+                    break;
+                case 10:
+                    System.out.println(31);
+                    break;
+                case 11:
+                    System.out.println(30);
+                    break;
+                case 12:
+                    System.out.println(31);
+                    break;
+            }
+        }
     }
 }
