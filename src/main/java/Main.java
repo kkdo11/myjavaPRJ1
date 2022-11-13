d01ebae87398f348aa7a434c657e0d7ef43d4c7c
kimdowon <kdw030612@gmail.com>
2022-11-13 14:07:57 +0900

    20221113

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
+        Scanner in = new Scanner(System.in);
 
-        int a=in.nextInt();
-        int b=in.nextInt();
-        int c=in.nextInt();
-        int s=b-c;
-        if (s>a){
-            System.out.println("advertise");
-        }else if(s==a){
-            System.out.printf("does not matter");
-        }else{
-            System.out.printf("do not advertise");
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        if (a + b > c) {
+            if (a == b && a == c) {
+                System.out.printf("정삼각형");
+            } else if (a == b || a == c || b == c) {
+                System.out.printf("이등변삼각형");
+            } else if ((a * a + b * b) == (c * c)) {
+                System.out.printf("직각삼각형");
+            }
+            else {
+                System.out.printf("삼각형");
+            }
+        } else {
+            System.out.printf("삼각형아님");
         }
     }
 }
